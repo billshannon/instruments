31e9ca29b13fd256e3df6a74d2c99054304f400f
Bill Shannon <bill@billshannon.com>
2015-10-27 12:11:55 -0600

    fix form handler

--- routes/instruments.js
+++ routes/instruments.js
@@ -28,7 +28,7 @@ router.route('/')
                         })
                     },
                     json: function () {
-                        res.json(infophotos)
+                        res.json(instruments)
                     }
                 });
             }
@@ -73,13 +73,18 @@ router.get('/new', function (req, res) {
 router.param('id', function (req, res, next, id) {
     mongoose.model('Instrument').findById(id, function (err, instrument) {
         if (err) {
-            console.log(id + ' was not found')
+            console.log(id + ' was not found');
             res.status(404);
+            var err = new Error('Not Found');
+            err.status = 404;
             res.format({
                 html: function () {
+                    next(err);
+                },
+                json: function () {
                     res.json({message: err.status + ' ' + err});
                 }
-            })
+            });
         } else {
             console.log(instrument);
             req.id = id;
@@ -109,82 +114,84 @@ router.route('/:id')
         });
     });
 
-// get the instrument by Mondo ID
-router.get('/:id/edit', function (req, res) {
-    mongoose.model('Instrument').findById(req.id, function (err, instrument) {
-        if (err) {
-            console.log('GET Error: There was a problem retrieving: ' + err);
-        } else {
-            console.log('GET Retrieving ID: ' + instrument._id);
-            res.format({
-                html: function () {
-                    res.render('instruments/show', {
-                        "instrument": instrument
-                    })
-                },
-                json: function () {
-                    res.json(instrument);
-                }
-            });
-        }
-    });
-});
-
-// PUT to update an instrument by ID
-router.put('/:id/edit', function (req, res) {
-    var make = req.body.make;
-    var model = req.body.model;
-    var year = req.body.year;
-    var price = req.body.price;
-    mongoose.model('Instrument').findByID(req.id, function (err, instrument) {
-        instrument.update({
-            make: make,
-            model: model,
-            year: year,
-            price: price
-        }, function (err, instrumentID) {
+// get the instrument by Mongo ID
+router.route('/:id/edit')
+    .get(function (req, res) {
+        mongoose.model('Instrument').findById(req.id, function (err, instrument) {
             if (err) {
-                res.send("There was a problem updating the instrument to the database: " + err);
+                console.log('GET Error: There was a problem retrieving: ' + err);
             } else {
+                console.log('GET Retrieving ID: ' + instrument._id);
                 res.format({
                     html: function () {
-                        res.redirect('/instruments/' + instrument._ID)
+                        res.render('instruments/edit', {
+                            title: 'Instrument ' + instrument._id,
+                            "instrument": instrument
+                        })
                     },
                     json: function () {
-                        res.json(instrument)
+                        res.json(instrument);
                     }
-                })
+                });
             }
-        })
+        });
     })
-});
 
-// delete an instrument by ID
-router.delete('/:id/edit', function (req, res) {
-    mongoose.model('Instrument').findById(req.id, function (err, instrument) {
-        if (err) {
-            return console.error(err);
-        } else {
-            instrument.remove(function (err, instrument) {
+// PUT to update an instrument by ID
+    .put(function (req, res) {
+        var make = req.body.make;
+        var model = req.body.model;
+        var year = req.body.year;
+        var price = req.body.price;
+        mongoose.model('Instrument').findByID(req.id, function (err, instrument) {
+            instrument.update({
+                make: make,
+                model: model,
+                year: year,
+                price: price
+            }, function (err, instrumentID) {
                 if (err) {
-                    return console.error(err)
+                    res.send("There was a problem updating the instrument to the database: " + err);
                 } else {
-                    console.log('DELETE removing ID: ' + instrument._id);
                     res.format({
                         html: function () {
-                            res.redirect('/instruments')
+                            res.redirect('/instruments/' + instrument._ID)
                         },
                         json: function () {
-                            res.json({
-                                message: 'deleted',
-                                item: instrument
-                            });
+                            res.json(instrument)
                         }
-                    });
+                    })
                 }
-            });
-        }
+            })
+        })
+    })
+
+// delete an instrument by ID
+    .delete(function (req, res) {
+        mongoose.model('Instrument').findById(req.id, function (err, instrument) {
+            if (err) {
+                return console.error(err);
+            } else {
+                instrument.remove(function (err, instrument) {
+                    if (err) {
+                        return console.error(err)
+                    } else {
+                        console.log('DELETE removing ID: ' + instrument._id);
+                        res.format({
+                            html: function () {
+                                res.redirect('/instruments')
+                            },
+                            json: function () {
+                                res.json({
+                                    message: 'deleted',
+                                    item: instrument
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
     });
-});
 
 module.exports = router;
